121f66e3a29ffb55a465a50041903374d6e83876
kouff <MY_NAME@example.com>
2021-09-18 14:00:55 +0300

    improve models

--- api/models.py
+++ api/models.py
@@ -6,17 +6,20 @@ class Portfolio(models.Model):
     name = models.CharField(max_length=150)
     description = models.TextField()
     owner = models.ForeignKey(User, related_name='portfolios', on_delete=models.CASCADE)
+    creation_date = models.DateTimeField(auto_now_add=True)
     # images
 
 
 class Image(models.Model):
     name = models.CharField(max_length=150)
     description = models.TextField()
+    image = models.ImageField(upload_to='images/', default='')
     portfolio = models.ForeignKey(Portfolio, related_name='images', on_delete=models.CASCADE)
+    creation_date = models.DateTimeField(auto_now_add=True)
     # comments
 
 
 class Comment(models.Model):
     text = models.TextField()
-    image = models.ForeignKey(Portfolio, related_name='comments', on_delete=models.CASCADE)
-    datetime = models.DateTimeField(auto_now_add=True)
+    image = models.ForeignKey(Image, related_name='comments', on_delete=models.CASCADE)
+    creation_date = models.DateTimeField(auto_now_add=True)
